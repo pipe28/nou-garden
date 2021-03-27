ccada829825e0c785f2377dc9b15072206c734fb
pipe28 <44620730+pipe28@users.noreply.github.com>
2021-03-26 23:20:03 -0300

    Add files via upload

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -11,9 +11,9 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "felipe2889@hotmail.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Website Contact Form:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
+$to = "info@nougarden.cl"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$subject = "Formulario Web:  $name";
+$body = "Mensaje desde formulario web en nougarden.cl.\n\n"."Detalles:\n\nCliente: $name\n\nCorreo: $email\n\nFono: $phone\n\nMensaje:\n$message";
 $header = "From: no-responder@nougarden.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
